b8782a715cf6e0381d13831d42c9b845a2f476cf
Severin Smith <info@smallbutdigital.com>
2012-05-07 12:25:27 -0400

    Little error

--- software/src/propinquity/hardware/ProxEventListener.java
+++ software/src/propinquity/hardware/ProxEventListener.java
@@ -1,15 +1,5 @@
 package propinquity.hardware;
 
-import java.util.*;
-
-import processing.core.*;
-import processing.serial.*;
-import controlP5.*;
-
-import com.rapplogic.xbee.api.*;
-
-import propinquity.*;
-
 public interface ProxEventListener {
 
 	public void proxEvent(Glove glove);
