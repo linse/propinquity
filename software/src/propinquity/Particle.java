9f3a0c1c3fc6c8395ea13b7a2a8041f1e3868df2
Stephane Beniak <stephane.beniak@gmail.com>
2012-05-09 15:19:58 -0400

    Forgetting metaballs for now, focus on getting gameplay working, will make it pretty later.

--- software/src/propinquity/Particle.java
+++ software/src/propinquity/Particle.java
@@ -17,7 +17,7 @@ public class Particle {
 
 	public Vec2 position;
 	public float scale;
-	
+
 	private Body body;
 	private CircleDef shape;
 
@@ -36,7 +36,7 @@ public class Particle {
 		scale = 0.5f;
 
 		shape = new CircleDef();
-		shape.radius = parent.box2d.scalarPixelsToWorld((texture.width) * scale / 2f);
+		shape.radius = parent.box2d.scalarPixelsToWorld((texture.width - 22) * scale / 2f);
 		shape.density = 1.0f;
 		shape.friction = 0.01f;
 		shape.restitution = 0.3f;
@@ -56,7 +56,7 @@ public class Particle {
 	public Body getBody() {
 		return body;
 	}
-	
+
 	public void update() {
 
 	}
