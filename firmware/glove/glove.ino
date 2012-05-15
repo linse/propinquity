a76679000f584834d5facb55a742c3133e4d0cbe
Vincent de Belleval <2000db@gmail.com>
2012-05-15 15:27:09 -0400

    glove

--- firmware/glove/glove.ino
+++ firmware/glove/glove.ino
@@ -7,24 +7,13 @@
 #include <Timer.h>
 
 /* ---- Pin List ---- */
-
-#define RED_LED_PIN    8
-#define GREEN_LED_PIN  9
-#define BLUE_LED_PIN   10
-#define VIBE_PIN       6
-#define STATUS_LED_PIN 7
-#define PROX_PIN       4
+#define VIBE_PIN       3
+#define STATUS_LED_PIN 13
 
 /* ---- Protocol ---- */
 #define BASE_ADDR      0
-
 #define PROX_PACKET    1
-
 #define CONF_PACKET    2
-
-#define COLOR_PACKET   3
-#define COLOR_DUTY_PACKET     4
-#define COLOR_PERIOD_PACKET   5
 
 #define VIBE_PACKET    6
 #define VIBE_DUTY_PACKET      7
@@ -46,35 +35,18 @@ Rx16Response rx = Rx16Response();
 uint8_t send_flag = 0;
 uint8_t send_period = 10;
 
-/* ---- Vibe/LEDs ----- */
+/* ---- Vibe ----- */
 
 uint8_t active = 0;
-
-uint8_t rgb[3] = {0, 0, 0};
-
-boolean led_on = true;
-uint8_t led_duty = 0;
-uint8_t led_period = 0;
-
 int vibe_level = 0;
-
 boolean vibe_on = true;
 uint8_t vibe_duty = 0;
 uint8_t vibe_period = 0;
 
-int prox_val = 0;
-int prox_adju = 0; //250
-
-
 void setup() {
-	pinMode(RED_LED_PIN, OUTPUT);
-	pinMode(BLUE_LED_PIN, OUTPUT);
-	pinMode(GREEN_LED_PIN, OUTPUT);
-	pinMode(STATUS_LED_PIN, OUTPUT);
 	pinMode(VIBE_PIN, OUTPUT);
 
 	setActive(0);
-	color(0, 0, 0);
 	vibe(0);
 
 	t.every(10, timerCallback);
@@ -87,10 +59,6 @@ void setup() {
  */
 void timerCallback() {
 	if((time_counter % send_period) == 0) send_flag = 1; // Every 10ms
-	
-	if(led_period == 0 || led_duty == 255) led_on = true;
-	else if((time_counter % led_period) < (uint8_t)(led_duty*led_period/255)) led_on = true;
-	else led_on = false;
 
 	if(vibe_period == 0 || vibe_duty == 255) vibe_on = true;
 	else if((time_counter % vibe_period) < (uint8_t)(vibe_duty*vibe_period/255)) vibe_on = true;
@@ -112,27 +80,9 @@ void loop() {
 			parse_data(rx.getData(), rx.getDataLength());
 		}
 	}
-
-	if(active && send_flag) { //This is done with a flag since the send_data won't work from inside an interrupt
-		prox_val = analogRead(PROX_PIN);
-
-		if(prox_val < prox_adju) prox_val = 0;
-		else prox_val -= prox_adju;
-
-		send_data();
-
-		send_flag = 0;
-	} else { // Turn off when inactive? sure
-		// reset();
-	}
-
-	if(active) {
-		updateVibe();
-		updateLEDs();
-	} else {
-		vibe(0);
-		color(0, 0, 0);
-	}
+	
+	if(active) updateVibe();
+	else vibe(0);
 }
 
 void reset() {
@@ -142,41 +92,27 @@ void reset() {
 	vibe_duty = 0;
 	vibe_period = 0;
 
-	rgb[0] = rgb[1] = rgb[2] = 0;
-
-	led_on = false;
-	led_duty = 0;
-	led_period = 0;
-
-	color(0, 0, 0);
 	vibe(0);
 }
 
 /* ---- Xbee ---- */
 
-void send_data() {
-	uint8_t outPacket[3];
+/* No sending for the glove... add status packets? */
+/* void send_data() { */
+/* 	uint8_t outPacket[3]; */
 
-	outPacket[0] = PROX_PACKET;
-	outPacket[1] = uint8_t(prox_val >> 8);
-	outPacket[2] = uint8_t(prox_val);
+/* 	outPacket[0] = PROX_PACKET; */
+/* 	outPacket[1] = uint8_t(prox_val >> 8); */
+/* 	outPacket[2] = uint8_t(prox_val); */
 
-	tx = Tx16Request(BASE_ADDR, outPacket, 3);
+/* 	tx = Tx16Request(BASE_ADDR, outPacket, 3); */
 
-	xbee.send(tx);
-}
+/* 	xbee.send(tx); */
+/* } */
 
 void parse_data(uint8_t* data, uint8_t len) {
 	if(data[0] == CONF_PACKET && len > 1) {
 		setActive(data[1]);
-	} else if(data[0] == COLOR_PACKET && len > 3) {
-		rgb[0] = data[1];
-		rgb[1] = data[2];
-		rgb[2] = data[3];
-	} else if(data[0] == COLOR_DUTY_PACKET && len > 1) {
-		led_duty = data[1];
-	} else if(data[0] == COLOR_PERIOD_PACKET && len > 1) {
-		led_period = data[1];
 	} else if(data[0] == VIBE_PACKET && len > 1) {
 		vibe_level = data[1];
 	} else if(data[0] == VIBE_DUTY_PACKET && len > 1) { 
@@ -195,23 +131,11 @@ void setActive(int val) {
 		statusLED(0);
 	}
 }
-/* ---- Blinking ---- */
 
+/* ---- Blinking ---- */
 void updateVibe() {
 	if(vibe_on) vibe(vibe_level);
 	else vibe(0);
-}
-
-void updateLEDs() {
-	if(led_on) color(rgb[0], rgb[1], rgb[2]);
-	else color(0, 0, 0);
-}
-
-/* ---- Low Level ---- */
-void color(unsigned char red, unsigned char green, unsigned char blue) {
-	analogWrite(RED_LED_PIN, 255-red);	 
-	analogWrite(BLUE_LED_PIN, 255-blue);
-	analogWrite(GREEN_LED_PIN, 255-green);
 }
 
 void vibe(unsigned char level) {
