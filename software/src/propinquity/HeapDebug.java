eccc4669d3d8d8c14542d551216415e5ae1be443
Severin Smith <info@smallbutdigital.com>
2012-05-16 19:06:15 -0400

    Made a heap size debugger
    This lets us check box2d and minim aren't leaking memory somewhere

---
+++ software/src/propinquity/HeapDebug.java
@@ -0,0 +1,49 @@
+package propinquity;
+
+public class HeapDebug implements Runnable {
+
+	static int MB = 1024*1024;
+
+	Runtime runtime;
+
+	boolean running;
+	Thread debugThread;
+
+	public HeapDebug() {
+		runtime = Runtime.getRuntime();
+	}
+
+	public void run() {		
+		//Getting the runtime reference from system
+		while(running) {
+			System.out.println("Heap Stats [MB]:");
+			System.out.println("\tUsed:" + (float)(runtime.totalMemory() - runtime.freeMemory()) / MB);
+			System.out.println("\tFree Memory:" + (float)runtime.freeMemory() / MB);
+			System.out.println("\tTotal Memory:" + (float)runtime.totalMemory() / MB);
+			System.out.println("\tMax Memory:" + (float)runtime.maxMemory() / MB);
+
+			try {
+				Thread.sleep(1000);
+			} catch(Exception e) {
+
+			}
+		}
+	}
+
+	public boolean isRunning() {
+		return running;
+	}
+
+	public void stop() {
+		running = false;
+		if(debugThread != null) while(debugThread.isAlive()) Thread.yield();
+	}
+
+	public void start() {
+		running = true;
+		debugThread = new Thread(this);
+		debugThread.setDaemon(true);
+		debugThread.start();
+	}
+
+}
