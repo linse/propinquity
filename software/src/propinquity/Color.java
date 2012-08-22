00c75c46502736e9ea77e8f64a0edd80f2e9b51e
Severin Smith <info@smallbutdigital.com>
2012-08-22 14:36:24 -0400

    Reduce teal brightness for less false positives

--- software/src/propinquity/Color.java
+++ software/src/propinquity/Color.java
@@ -169,7 +169,7 @@ public class Color {
 	 * @return The color "teal" as a Color object.
 	 */
 	public static Color teal() {
-		return new Color(0, 255, 255);
+		return new Color(0, 200, 200);
 	}
 
 }
