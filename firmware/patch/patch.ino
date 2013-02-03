bb829f2d9823ae25d0f6b640db5a802760d4f7c7
Severin Smith <info@smallbutdigital.com>
2013-02-03 17:44:34 -0500

    Working on updated firmware for patch with accel

--- firmware/patch/patch.ino
+++ firmware/patch/patch.ino
@@ -3,28 +3,21 @@
  *
  */
 
-#include <XBee.h>
 #include <Timer.h>
 #include <Wire.h>
+#include <XBee.h>
 #include "MMA8452Q.h"
-
-/* ---- Pin List ---- */
-
-#define RED_LED_PIN    10
-#define GREEN_LED_PIN  9
-#define BLUE_LED_PIN   5
-
-#define VIBE_PIN       6
-
-#define PROX_PIN       A7 // 21
+// #include <TimerOne.h>
 
 /* ---- Protocol ---- */
+
+//TODO: Use enum instead and use case statement below
 
 #define BASE_ADDR      0
 
 #define PROX_PACKET    1
 
-#define CONF_PACKET    2
+#define MODE_PACKET    2
 #define CLEAR_PACKET   3
 
 #define COLOR_PACKET   4
@@ -35,29 +28,36 @@
 #define VIBE_DUTY_PACKET      8
 #define VIBE_PERIOD_PACKET    9
 
+/* ---- Pin List ---- */
+
+#define RED_LED_PIN    10
+#define GREEN_LED_PIN  9
+#define BLUE_LED_PIN   5
+
+#define VIBE_PIN       6
+
+#define PROX_PIN       A7 // Pin 21 Not available on normal arduino
+
 /* ---- Accel ---- */
 
 #define ADDR_DEVICE 0x1C
 
-/* ---- Timer ---- */
+uint8_t accel_ok = 0;
+uint8_t accel_flag = 0;
+uint8_t accel_period = 10;
 
- // Timer
- Timer t;
+/* ---- Prox ----*/
 
- uint16_t time_counter = 0;
+#define PROX_AVG_LEN 8
 
-/* ---- XBee/Wireless ---- */
+uint8_t prox_val_pointer = 0;
+uint16_t avg_prox_val = 0;
+uint16_t prox_val[PROX_AVG_LEN];
 
-XBee xbee = XBee();
-Tx16Request tx;
-Rx16Response rx = Rx16Response();
-
-uint8_t send_flag = 0;
-uint8_t send_period = 10;
+uint8_t prox_flag = 0;
+uint8_t prox_period = 10;
 
 /* ---- Vibe/LEDs ----- */
-
-uint8_t active = 0;
 
 uint8_t rgb[3] = {0, 0, 0};
 
@@ -71,85 +71,53 @@ boolean vibe_on = true;
 uint8_t vibe_duty = 0;
 uint8_t vibe_period = 0;
 
-/* ---- Prox ----*/
+/* ---- Global ---- */
 
-#define PROX_AVG_LEN 8
+#define EN_MODE 0
+#define PROX_MODE 1
+#define ACCEL_XYZ_MODE 2
 
-uint8_t prox_val_pointer = 0;
-uint16_t avg_prox_val = 0;
-uint16_t prox_val[PROX_AVG_LEN];
+uint8_t mode = 0;
+
+/* ---- Timer ---- */
+
+Timer t;
+uint16_t time_counter = 0;
+
+/* ---- XBee/Wireless ---- */
+
+XBee xbee = XBee();
+Tx16Request tx;
+Rx16Response rx = Rx16Response();
 
 void setup() {
 	pinMode(RED_LED_PIN, OUTPUT);
 	pinMode(BLUE_LED_PIN, OUTPUT);
 	pinMode(GREEN_LED_PIN, OUTPUT);
+
 	pinMode(VIBE_PIN, OUTPUT);
 
-	setActive(0);
-	color(0, 0, 0);
-	vibe(0);
+	pinMode(PROX_PIN, INPUT);
 
-	for(uint8_t i = 0;i < PROX_AVG_LEN;i++) prox_val[i] = 0;
+	pinMode(SDA, INPUT);
+	pinMode(SCL, INPUT);	
+
+	reset();
+	setMode(0);
+
+	accel_reset();
+	accel_config();
 
 	t.every(10, timerCallback);
 
+	// Timer1.initialize(1000); //1000 microseconds -> 10 milliseconds
+	// Timer1.attachInterrupt(timerCallback); // attach the service routine here
+
+	// Serial.begin(9600);
 	xbee.begin(38400);
-
-	Wire.begin();
-
-	Wire.beginTransmission(ADDR_DEVICE);
-	Wire.write(CTRL_REG2);
-	Wire.write(1 << RST);
-	Wire.endTransmission();
-	
-	delay(1000);
-	
-	Wire.beginTransmission(ADDR_DEVICE);
-	Wire.write(XYZ_DATA_CFG);
-	Wire.write(1 << FS1 | 1 << FS0);
-	Wire.endTransmission();
-	
-	Wire.beginTransmission(ADDR_DEVICE);
-	Wire.write(CTRL_REG1);
-	Wire.write(1 << ACTIVE);
-	Wire.endTransmission();
-
-	Serial.begin(9600);
 }
-
-/**
- * Set the data out flag every ~10ms.
- */
-void timerCallback() {
-	if((time_counter % send_period) == 0) send_flag = 1; // Every 10ms
-	
-	if(led_period == 0 || led_duty == 255) led_on = true;
-	else if((time_counter % led_period) < (uint8_t)(led_duty*led_period/255)) led_on = true;
-	else led_on = false;
-
-	if(vibe_period == 0 || vibe_duty == 255) vibe_on = true;
-	else if((time_counter % vibe_period) < (uint8_t)(vibe_duty*vibe_period/255)) vibe_on = true;
-	else vibe_on = false;
-	
-	time_counter++;
-}
-
 
 void loop() {
-	// Wire.requestFrom(ADDR_DEVICE, 7);
-	
-	// byte status = Wire.read();
-
-	// int16_t data[6];
-	
-	// for(int i = 0;i < 6;i++) {
-	// 	uint16_t value = (Wire.read() << 4) | (Wire.read() >> 4);
-	// 	if(value & 0x0800) value |= 0xF000;
-	// 	data[i] = value;
-	// }
-	
-	// sendXYZ(data[0], data[1], data[2]);
-	
 	t.update();
 
 	xbee.readPacket(); // Read packet, return after 500ms timeout
@@ -162,43 +130,76 @@ void loop() {
 		}
 	}
 
-	if(active && send_flag) { //This is done with a flag since the send_data won't work from inside an interrupt
-		prox_val[prox_val_pointer] = analogRead(PROX_PIN);
-		prox_val_pointer = (prox_val_pointer+1)%PROX_AVG_LEN;
-
-		avg_prox_val = 0;
-		for(uint8_t i = 0;i < PROX_AVG_LEN;i++) {
-			avg_prox_val += prox_val[i];
-		}
-
-		avg_prox_val = avg_prox_val >> 3; //Divide by 18
-
-		send_data();
-
-		send_flag = 0;
-	} else { // Turn off when inactive? sure
-		// reset();
-	}
-
-	if(active) {
-		updateVibe();
-		updateLEDs();
-	} else {
+	if(!(mode & (1 << EN_MODE))) {
+		//Disabled, lights and vibe off
 		vibe(0);
 		color(0, 0, 0);
+	} else {
+		if((mode & (1 << PROX_MODE)) && prox_flag) {
+			//Prox enable
+			prox_val[prox_val_pointer] = analogRead(PROX_PIN);
+			prox_val_pointer = (prox_val_pointer+1)%PROX_AVG_LEN;
+
+			avg_prox_val = 0;
+			for(uint8_t i = 0;i < PROX_AVG_LEN;i++) {
+				avg_prox_val += prox_val[i];
+			}
+
+			avg_prox_val = avg_prox_val >> 3; //Divide by 8
+
+			send_data();
+
+			prox_flag = 0;
+		}
+
+		if((mode & (1 << ACCEL_XYZ_MODE)) && accel_flag) {
+			// Wire.requestFrom(ADDR_DEVICE, 7);
+			
+			// byte status = Wire.read();
+
+			// int16_t data[6];
+			
+			// for(int i = 0;i < 6;i++) {
+			// 	uint16_t value = (Wire.read() << 4) | (Wire.read() >> 4);
+			// 	if(value & 0x0800) value |= 0xF000;
+			// 	data[i] = value;
+			// }
+			
+			// sendXYZ(data[0], data[1], data[2]);
+		}
+
+		updateVibe();
+		updateLEDs();
 	}
 }
 
-void sendXYZ(int x, int y, int z) {
-	Serial.print(x);
-	Serial.print(" - ");
-	Serial.print(y);
-	Serial.print(" - ");
-	Serial.print(z);
-	Serial.print("\n");
+/**
+ * Set the data out flag every ~10ms.
+ */
+void timerCallback() {
+	if((time_counter % prox_period) == 0) prox_flag = 1; // Every 10ms
+	
+	if(led_period == 0 || led_duty == 255) led_on = true;
+	else if((time_counter % led_period) < (uint8_t)(led_duty*led_period/255)) led_on = true;
+	else led_on = false;
+
+	if(vibe_period == 0 || vibe_duty == 255) vibe_on = true;
+	else if((time_counter % vibe_period) < (uint8_t)(vibe_duty*vibe_period/255)) vibe_on = true;
+	else vibe_on = false;
+	
+	time_counter++;
 }
 
+/**
+ * Reset patch
+ */
 void reset() {
+	accel_flag = 0;
+
+	prox_flag = 0;
+	avg_prox_val = 0;
+	for(uint8_t i = 0;i < PROX_AVG_LEN;i++) prox_val[i] = 0;
+
 	vibe_level = 0;
 
 	vibe_on = false;
@@ -213,6 +214,121 @@ void reset() {
 
 	color(0, 0, 0);
 	vibe(0);
+}
+
+/* ---- Accel ---- */
+
+void accel_reset() {
+	//Reset the accelerometer
+	accel_reg_write(CTRL_REG2, (1 << RST));
+
+	//Wait for reset to complete (blink yellow for wait state)
+	int timer = 0;
+
+	while(timer < 1000) { //Wait up to 1 second for reset
+		if((accel_reg_read(CTRL_REG2) & (1 << RST)) != 0) {
+			accel_ok = 1;
+			break;
+		}
+
+		if(timer % 200 < 100) color(20, 20, 0); //Blink yelow
+		else color(0, 0, 0);
+
+		timer++;
+		delay(1);
+	}
+
+	if(accel_ok) {
+		//Green for ok
+		color(0, 40, 0);
+		delay(250);
+	} else {
+		//Red for fail
+		color(40, 0, 0);
+		delay(250);
+	}
+}
+
+void accel_config() {	
+	//IMPORTANT: all registers must be set with the accelerometer in STANDBY
+
+	accel_enable(0);
+
+	//Note: I'm currently wiping out any other config in CTRL_REG1 by doing it this way
+
+	// reg_write(XYZ_DATA_CFG, (1 << HPF_OUT) | (FULL_SCALE_RANGE << FS0)); //Set range +-4g, output high pass data
+	accel_reg_write(XYZ_DATA_CFG, (1 << FS1 | 1 << FS0)); //Set range +-4g, output high pass data
+	// accel_reg_write(HP_FILTER_CUTOFF, (HIGH_PASS_CONF << SEL0)); //High pass second lowest setting
+
+	//Motion
+
+	//Set the freefall/motin configuration registers to detect motion events on XYZA
+	// accel_reg_set(FF_MT_CFG,
+	// 	(1 << FF_MT_ELE) |
+	// 	(1 << OAE) | //Dection mode 1,1 (see datasheet)
+	// 	(1 << ZEFE) |
+	// 	(1 << YEFE) |
+	// 	(1 << XEFE) //Enable XYZ axis
+	// );
+	// accel_reg_set(FF_MT_THS, (uint8_t)(
+	// 	(0 << DBCNTM) | //Decrement debounce counter when motion falls below threshold
+	// 	((MOTION_THRESHOLD) << THS) //Motion threshold (7 bits)
+	// ));
+	
+	// accel_reg_set(FF_MT_COUNT, MOTION_DEBOUNCE_COUNT); //Debounce count
+
+	// // Transient
+
+	// //Set the freefall/motin configuration registers to detect motion events on XYZA
+	// accel_reg_set(TRANSIENT_CFG,
+	// 	(1 << T_ELE) | //Event latching, clean on read
+	// 	(1 << ZTEFE) |
+	// 	(1 << YTEFE) |
+	// 	(1 << XTEFE) | //Enable XYZ axis
+	// 	(0 << HPF_BYP) //Keep the high pass
+	// );
+	// accel_reg_set(TRANSIENT_THS, (uint8_t)(
+	// 	(0 << DBCNTM) | //Decrement debounce counter when motion falls below threshold
+	// 	((TRANS_THRESHOLD) << THS) //Motion threshold (7 bits)
+	// ));
+	
+	// accel_reg_set(TRANSIENT_COUNT, TRANS_DEBOUNCE_COUNT); //Debounce count
+
+	accel_enable(1);
+}
+
+void accel_enable(uint8_t enable) {
+	uint8_t cur_val = accel_reg_read(CTRL_REG1);
+
+	if(enable) {
+		accel_reg_write(CTRL_REG1, cur_val | (1 << ACTIVE)); //Active mode
+	} else {
+		accel_reg_write(CTRL_REG1, cur_val & ~(0 << ACTIVE)); //Standby mode
+	}
+}
+
+void accel_reg_write(uint8_t reg_addr, uint8_t data) {
+	Wire.beginTransmission(ADDR_DEVICE);
+	Wire.write(reg_addr);
+	Wire.write(data);
+	Wire.endTransmission();
+}
+
+uint8_t accel_reg_read(uint8_t reg_addr) { 
+	Wire.beginTransmission(ADDR_DEVICE);
+	Wire.write(reg_addr);
+	Wire.endTransmission(false); //Do not send stop condition, this will send a repeat start instead
+	Wire.requestFrom(ADDR_DEVICE, 1);
+	byte status = Wire.read();
+}
+
+void sendXYZ(int x, int y, int z) {
+	Serial.print(x);
+	Serial.print(" - ");
+	Serial.print(y);
+	Serial.print(" - ");
+	Serial.print(z);
+	Serial.print("\n");
 }
 
 /* ---- Xbee ---- */
@@ -230,19 +346,10 @@ void send_data() {
 }
 
 void parse_data(uint8_t* data, uint8_t len) {
-	if(data[0] == CONF_PACKET && len > 1) {
-		setActive(data[1]);
+	if(data[0] == MODE_PACKET && len > 1) {
+		setMode(data[1]);
 	} else if(data[0] == CLEAR_PACKET) {
-		rgb[0] = 0;
-		rgb[1] = 0;
-		rgb[2] = 0;
-
-		led_duty = 0;
-		led_period = 0;
-
-		vibe_level = 0;
-		vibe_period = 0;
-		vibe_duty = 0;
+		reset();
 	} else if(data[0] == COLOR_PACKET && len > 3) {
 		rgb[0] = data[1];
 		rgb[1] = data[2];
@@ -260,18 +367,17 @@ void parse_data(uint8_t* data, uint8_t len) {
 	} 
 }
 
-void setActive(uint8_t val) {
-	if(val) {
-		active = 1;
+void setMode(uint8_t new_mode) {
+	if(new_mode) {
+		mode = new_mode;
 	} else {
-		active = 0;
-		if(!active) {
-			for(uint8_t i = 0;i < PROX_AVG_LEN;i++) {
-				prox_val[i] = 0;
-			}
+		mode = 0;
+		for(uint8_t i = 0;i < PROX_AVG_LEN;i++) {
+			prox_val[i] = 0;
 		}
 	}
 }
+
 /* ---- Blinking ---- */
 
 void updateVibe() {
