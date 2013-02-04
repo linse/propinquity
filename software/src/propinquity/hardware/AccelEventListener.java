3a8dcb18528222ab2fc8090f596689f87045300f
Severin Smith <info@smallbutdigital.com>
2013-02-04 02:45:26 -0500

    Added missing file

---
+++ software/src/propinquity/hardware/AccelEventListener.java
@@ -0,0 +1,11 @@
+package propinquity.hardware;
+
+/**
+ * Any class implementing this interface can register with a class implementing {@link propinquity.hardware.HardwareInterface} in order to receive callbacks when new accel data is recieved.
+ *
+ */
+public interface AccelEventListener {
+
+	public void accelXYZEvent(Patch patch);
+
+}
